aa0f78e5b076802ead87ef14a820a231ae659be3
Nick <nickfarrell@gmail.com>
2018-05-03 21:12:02 -0700

    start instantiateContract method

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-import getWeb3 from './utils/getWeb3'
+import React, { Component } from 'react';
+//import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import MultiNumberBettingV7Contract from '../build/contracts/MultiNumberBettingV7.json';
+import getWeb3 from './utils/getWeb3';
 
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
+import './css/oswald.css';
+import './css/open-sans.css';
+import './css/pure-min.css';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      ContractInstance: null,
       storageValue: 0,
       web3: null
     }
@@ -36,34 +38,24 @@ class App extends Component {
   }
 
   instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
 
-    const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const contract = require('truffle-contract');
+    const betting = contract(MultiNumberBettingV7Contract);
+    betting.setProvider(this.state.web3.currentProvider);
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    let bettingInstance;
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+      betting.deployed().then((instance) => {
+        bettingInstance = instance;
+        // store contract instance to state variable
+        return this.setState({ ContractInstance: bettingInstance });
+      }).then(() => {
+        // watch for contract events
+        return this.doContractEventWatchStart();
+      }) // figure out what to chain on to this part later
     })
   }
 
@@ -127,14 +119,14 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+            <a href="#" className="pure-menu-heading pure-menu-link">Simple Ethereum Betting ÐApp</a>
         </nav>
 
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
+              <h1>FEELING LUCKY PUNK?</h1>
+              <p>Put it on the line.</p>
               <h2>Smart Contract Example</h2>
               <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
               <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
